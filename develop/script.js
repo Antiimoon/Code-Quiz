3e7dd3f21e0e16a288c3fca1a440b26c058390dc
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2021-12-11 18:27:51 -0800

    multiple choices added, bugs fixed

--- develop/script.js
+++ develop/script.js
@@ -1,16 +1,34 @@
-// https://opentdb.com/api.php?amount=20
+// Quiz Section
+var quiz = document.querySelector("quiz-body");
+var timer = document.querySelector("#timer-sec");
 
-const _question = document.getElementById('question');
-const _options = document.querySelector('.quiz-options');
+// Question Section
+var question = document.querySelector("#question");
 
+// Multiple Choices of Questions
+var option1 = document.querySelector("#option1");
+var option2 = document.querySelector("#option2");
+var option3 = document.querySelector("#option3");
+var option4 = document.querySelector("#option4");
 
-async function loadQuestion(){
-    const APIUrl = 'https://opentdb.com/api.php?amount=1';
-    const result = await fetch(`${APIUrl}`);
-    const data = await result.json();
-    //console.log(data.results[0]);
-    showQuestion(data.results[0]);
-}
+// Correct and Next
+var correct = document.querySelector("correct-score");
+var nextBtn = document.querySelector("#next");
 
-function showQuestion(data){
-}
+//Result Section
+var result = document.querySelector("result");
+var score = document.querySelector("#high-score");
+var initial = document.querySelector("#initials");
+var tryAgainBtn = document.querySelector("#try-again");
+
+// Timer for Quiz
+let countDown = () => {
+    if (timer === 20) {
+        clearInterval(interval);
+    } else {
+        timer++;
+        console.log(timer);
+    }
+};
+
+setInterval(countDown, 1000);
