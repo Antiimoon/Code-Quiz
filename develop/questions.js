61806befa0e5cc0cf4e7417dc2d77c90f0645058
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2021-12-12 20:24:53 -0800

    two js

--- develop/questions.js
+++ develop/questions.js
@@ -1,81 +1,84 @@
-let MCQS = [{
-    question: "Inside which HTML element do we put the JavaScript?",
-    choice1: "<js>",
-    choice2: "<script>",
-    choice3: "<scripting>",
-    choice4: "<javascript>",
-    answer: "2. <script>",
-},
-    {
-        question: "How do you create a function in JavaScript?",
-    choice1: "function myFunction()",
-    choice2: "function:myFunction()",
-    choice3: "function = myFunction()",
-    choice4: "myFunction=",
-    answer: "1. function myFunction()",
-},
-    {
-        question: "How do you write an IF statement in JavaScript?",
-    choice1: "if i = 5",
-    choice2: "if i == 5 then",
-    choice3: "if i = 5 then",
-    choice4: " if (i == 5)",
-    answer: "4. if (i == 5)",
-},
-    {
-        question: "How does a FOR loop start?",
-    choice1: "for (i <= 5; i++)",
-    choice2: "for i = 1 to 5",
-    choice3: "for (i=0; i <= 5; i++)",
-    choice4: "for (i = 0; i <= 5)",
-    answer: "3. for (i=0; i <= 5; i++)",
-},
-    {
-        question: "How can you add a comment in a JavaScrpit?",
-    choice1: "//This is a comment",
-    choice2: "<!-- This is a comment -->",
-    choice3: "-This is a comment-",
-    choice4: "'This is a comment'",
-    answer: "1. //This is a comment",
-},
-    {
-        question: "What is the correct way to write a JavaScript array?",
-    choice1: "var colors = (1:"red", 2:"green", 3:"blue")",
-    choice2: "var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")",
-    choice3: "var colors = ["red", "green", "blue"]",
-    choice4: "var colors = "red", "green", "blue"",
-    answer: "3. var colors = ["red", "green", "blue"]",
-},
-    {
-        question: "How do you find the number with the highest value of x and y?",
-    choice1: "Math.ceil(x, y)",
-    choice2: "ceil(x, y)",
-    choice3: "top(x, y)",
-    choice4: "Math.max(x, y)",
-    answer: "4. Math.max(x, y)",
-},
-    {
-        question: "Which event occurs when the user clicks on an HTML element?",
-    choice1: "onmouseclick",
-    choice2: "onclick",
-    choice3: "onmouseover",
-    choice4: "clickedon",
-    answer: "2. onclick",
-},
-    {
-        question: "How do you declare a JavaScript variable?",
-    choice1: "var-thisName",
-    choice2: "var thisName",
-    choice3: "variable thisName",
-    choice4: "v thisName",
-    answer: "2. var thisName",
-},
-    {
-        question: "Which operator is used to assign a value to a variable?",
-    choice1: "-",
-    choice2: "*",
-    choice3: "~",
-    choice4: "=",
-    answer: "4. =",
-},
-]
+var question = new Array();
+var choices = new Array();
+var answers = new Array();
+
+    question[0] = "Inside which HTML element do we put the JavaScript?";
+    choices[0] = new Array();
+    choices[0][0] = "<js>";
+    choices[0][1] = "<script>";
+    choices[0][2] = "<scripting>";
+    choices[0][3] = "<javascript>";
+    answers [0] = choices[0][1];
+
+    question[1] = "How do you create a function in JavaScript?";
+    choices[1] = new Array();
+    choices[1][0] =  "function myFunction()";
+    choices[1][1] =  "function:myFunction()";
+    choices[1][2] = "function = myFunction()";
+    choices[1][3] =  "myFunction=",
+    answers [1] = choices[1][0];
+
+    question[2] = "How do you write an IF statement in JavaScript?";
+    choices[2] = new Array();
+    choices[2][0] =  "if i = 5";
+    choices[2][1] =  "if i == 5 then";
+    choices[2][2] = "if i = 5 then";
+    choices[2][3] =  " if (i == 5)";
+    answers [2] = choices[2][3];
+
+    question[3] ="How does a FOR loop start?";
+    choices[3] = new Array();
+    choices[3][0] =  "for (i <= 5; i++)";
+    choices[3][1] =  "for i = 1 to 5";
+    choices[3][2] = "for (i=0; i <= 5; i++)";
+    choices[3][3] =  "for (i = 0; i <= 5)";
+    answers [3] = choices[3][2];
+
+    question[4] = "How can you add a comment in a JavaScrpit?";
+    choices[4] = new Array();
+    choices[4][0] =  "//This is a comment";
+    choices[4][1] =  "<!-- This is a comment -->";
+    choices[4][2] = "-This is a comment-";
+    choices[4][3] =  "'This is a comment'";
+    answers [4] = choices[4][0];
+
+    question[5] = "How does a WHILE loop start";
+    choices[5] = new Array();
+    choices[5][0] =  "while (i <= 10; i++)";
+    choices[5][1] =  "while i = 1 to 10";
+    choices[5][2] = "while (i <= 10)";
+    choices[5][3] =  "while i === 10";
+    answers [5] = choices[5][2];
+
+    question[6] = "How do you find the number with the highest value of x and y?";
+    choices[6] = new Array();
+    choices[6][0] =  "Math.ceil(x, y)";
+    choices[6][1] =  "ceil(x, y)";
+    choices[6][2] = "top(x, y)";
+    choices[6][3] =  "Math.max(x, y)";
+    answers [6] = choices[6][3];
+
+    question[7] ="Which event occurs when the user clicks on an HTML element?";
+    choices[7] = new Array();
+    choices[7][0] =  "onmouseclick";
+    choices[7][1] =  "onclick";
+    choices[7][2] = "onmouseover";
+    choices[7][3] =  "clickedon";
+    answers [7] = choices[7][1];
+
+    question[8] = "How do you declare a JavaScript variable?";
+    choices[8] = new Array();
+    choices[8][0] =  "var-thisName";
+    choices[8][1] =  "var thisName";
+    choices[8][2] = "variable thisName";
+    choices[8][3] =  "v thisName";
+    answers [8] = choices[8][1];
+
+    question[9] = "Which operator is used to assign a value to a variable?";
+    choices[9] = new Array();
+    choices[9][0] =  "-";
+    choices[9][1] =  "*";
+    choices[9][2] = "~";
+    choices[9][3] =  "=";
+    answers [9] = choices[9][3];
+
